62f831b6c448ee5e6dcd532651e584064baf5956
Sam-Neale <sammneale@icloud.com>
2021-01-21 00:24:39 +1100

    Base2

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -2,3 +2,13 @@ setTimeout(() => {
     document.getElementById("loader").style.display = "none"
     document.getElementById("page").style.display = "block"
 }, 1500);
+
+
+function toggleHidden(id){
+    if (id == "footer") {
+        document.getElementById(id).classList.toggle("hidden")
+        document.getElementById("cover").classList.toggle("hidden")    
+    }else{
+    document.getElementById(id).classList.toggle("hidden")
+    }
+}
